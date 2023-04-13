82037cf51e23b5485d56b7f73824f4b8855eec1e
zenith1995 <versatile.skydev@gmail.com>
2023-04-13 15:09:12 -0400

    Updates

---
+++ precache-manifest.4509f4928ce56d2f729af0e76fdfc0d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8b331afd0bb0b0e0f9bb91804c6551",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "7426f821c0a8126775ee",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6c59528f60c5d84f76a0",
+    "url": "/react-frontend-dev-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "7426f821c0a8126775ee",
+    "url": "/react-frontend-dev-portfolio/static/js/2.718cc59c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.718cc59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c59528f60c5d84f76a0",
+    "url": "/react-frontend-dev-portfolio/static/js/main.fd810f50.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
